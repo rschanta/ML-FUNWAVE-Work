f8dc1e29299400ad4ce81de2e3ff7a62bfb09f2f
Ryan <rschanta@udel.edu>
2024-01-08 09:44:14 -0500

    Applied zero crossings

--- Key-Scripts/read_US_GC_profiles.m
+++ Key-Scripts/read_US_GC_profiles.m
@@ -1,11 +1,24 @@
 clear 
 clc
 close all
-profiles = {}; iter = 1;
+iter = 1;
 for j = 1:3786
     try
-        Xshore{iter} = h5read('./US_GC_data/Profile_data.h5',['/Profile/ID_',num2str(j),'/Xshore']);
-        Elevation{iter} = h5read('./US_GC_data/Profile_data.h5',['/Profile/ID_',num2str(j),'/Elevation']);
+        % Read in data
+        Xshore_raw = h5read('./US_GC_data/Profile_data.h5',['/Profile/ID_',num2str(j),'/Xshore']);
+        Elevation_raw = h5read('./US_GC_data/Profile_data.h5',['/Profile/ID_',num2str(j),'/Elevation']);
+
+        % Find zero crossings and index out
+        zero_cross = (Elevation_raw(1:end-1) .* Elevation_raw(2:end)) <= 0;
+        zero_cross_i = find(zero_cross);
+
+        Xshore_pro = Xshore_raw(1:zero_cross_i(1));
+        Elevation_pro = Elevation_raw(1:zero_cross_i(1));
+
+        % Append to cell arrays
+        Xshore{iter} = Xshore_pro;
+        Elevation{iter} = Elevation_pro;
+        
         iter = iter + 1;
     catch
         disp('Profile not found');
